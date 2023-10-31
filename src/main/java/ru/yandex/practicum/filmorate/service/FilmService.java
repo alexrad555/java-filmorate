ff44ca19bb80cc3f3dd18cacfc47229ff765fe42
Райдюк Александр <56934708+alexrad555@users.noreply.github.com>
2023-10-31 23:31:36 +0300

    sprint_10_done

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -13,7 +13,7 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
-public class FilmService extends AbstractService<Film>{
+public class FilmService extends AbstractService<Film> {
 
     private final FilmStorage filmStorage;
 
--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class UserService extends AbstractService<User>{
+public class UserService extends AbstractService<User> {
 
     private final UserStorage userStorage;
 
