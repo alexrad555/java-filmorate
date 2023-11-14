fcca4afaef915b024dc3fb0db12edb14ffd9dbc7
Райдюк Александр <56934708+alexrad555@users.noreply.github.com>
2023-11-14 21:11:51 +0300

    add_schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,48 +1,59 @@
-create table if not exists USERS
-(
-    id int not null primary key auto_increment,
-    email varchar(255) not null,
-    login varchar(255) not null,
-    birthday date,
-    deleted bool default false,
-    UNIQUE (email),
-    UNIQUE (login),
+CREATE TABLE "users" (
+  "id" id PRIMARY KEY,
+  "email" varchar,
+  "login" varchar,
+  "name" varchar,
+  "birthday" timestamp,
+  "deleted" bool
 );
 
-create table if not exists GENRES
-(
-    id int not null primary key auto_increment,
-    name varchar(255)
+CREATE TABLE "films" (
+  "id" id PRIMARY KEY,
+  "name" varchar,
+  "description" varchar,
+  "release_date" timestamp,
+  "rate" int,
+  "duration" int,
+  "mpa_id" int
 );
 
-create table if not exists MPA
-(
-    id int not null primary key auto_increment,
-    name varchar(255)
+CREATE TABLE "mpa" (
+  "id" id PRIMARY KEY,
+  "name" varchar
 );
 
-create table if not exists FILMS
-(
-    id int not null primary key auto_increment,
-    name varchar(255),
-    description varchar(255),
-    release_date date,
-    rate int,
-    duration int,
-    mpa_id int references MPA (id),
-    deleted bool default false
+CREATE TABLE "genres" (
+  "id" id PRIMARY KEY,
+  "name" varchar
 );
 
-create table if not exists FRIENDS
-(
-    user_id int not null references USERS (id),
-    friend_id int not null references USERS (id),
-    PRIMARY KEY (user_id, friend_id)
+CREATE TABLE "film_genres" (
+  "id" serial,
+  "film_id" int,
+  "genre_id" int
 );
 
-create table if not exists LIKES
-(
-    user_id int references USERS (id),
-    film_id int references FILMS (id),
-    PRIMARY KEY (user_id, friend_id)
+CREATE TABLE "friends" (
+  "id" int,
+  "user_id" int,
+  "friend_id" int
 );
+
+CREATE TABLE "likes" (
+  "user_id" int,
+  "film_id" int
+);
+
+ALTER TABLE "films" ADD FOREIGN KEY ("mpa_id") REFERENCES "mpa" ("id");
+
+ALTER TABLE "film_genres" ADD FOREIGN KEY ("film_id") REFERENCES "films" ("id");
+
+ALTER TABLE "film_genres" ADD FOREIGN KEY ("genre_id") REFERENCES "genres" ("id");
+
+ALTER TABLE "friends" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+ALTER TABLE "friends" ADD FOREIGN KEY ("friend_id") REFERENCES "users" ("id");
+
+ALTER TABLE "likes" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+ALTER TABLE "likes" ADD FOREIGN KEY ("film_id") REFERENCES "films" ("id");
